4493b7349734d6691e94a48d3c60519cb93a23f5
sanjanabontha <147245006+sanjanabontha@users.noreply.github.com>
2024-04-10 23:58:27 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,3 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+export const key = "{Your API Key Here}";
+export const base = "https://api.openweathermap.org/data/2.5/";
